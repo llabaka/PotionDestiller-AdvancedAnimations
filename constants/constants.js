9eb536ff5d93e820e02cb1b02a7501a23955c8bf
llabaka <lander.labaka@ikasle.aeg.eus>
2024-10-10 13:10:37 +0200

    [MAIN] New constant added to constants

--- constants/constants.js
+++ constants/constants.js
@@ -2,3 +2,4 @@ import { Dimensions } from "react-native";
 const { width, height } = Dimensions.get('window')
 export const SPACING = 10
 export const ITEM_SIZE = width * 0.75
+export const SPACER_ITEM_SIZE = (width - ITEM_SIZE)/2
