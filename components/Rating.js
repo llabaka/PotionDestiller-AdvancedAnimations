506733775a09f377577928a9c23a380a331a34ac
llabaka <lander.labaka@ikasle.aeg.eus>
2024-10-10 12:51:43 +0200

    [MAIN] Stars are working as intended

--- components/Rating.js
+++ components/Rating.js
@@ -9,8 +9,10 @@ const Container = styled.View`
     justify-content: center;
 `
 export default function Rating({rating}){
+    console.log("RATING: " + rating);
+    
     const totalOfFullStars = Math.floor(rating / 2)
-    const starOutLineArray = Array(5 - totalOfFullStars).fill('star-outline')
+    const starOutLineArray = Array(5 - totalOfFullStars).fill('star-o')
     const fullStarArray = Array(totalOfFullStars).fill('star')
     const ratingStars = [...fullStarArray, ...starOutLineArray]
 
